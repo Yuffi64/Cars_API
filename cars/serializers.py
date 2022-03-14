a55aca96be9e30bb5c56fc6057c98eacf8f3560b
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-03-14 16:09:27 -0500

    created car serializer class and file.

---
+++ cars/serializers.py
@@ -0,0 +1,8 @@
+import imp
+from rest_framework import Serializer
+from models import Car
+
+class CarSerializer(Serializer.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['id','make','model','year','price']
