acdaadd78079f16a304644228a192730aaa1dde3
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-03-14 21:27:51 -0500

    created admin file + added cars in url website.

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,8 +1,7 @@
-import imp
-from rest_framework import Serializer
-from models import Car
+from rest_framework import serializers
+from .models import Car
 
-class CarSerializer(Serializer.ModelSerializer):
+class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ['id','make','model','year','price']
